fcbcd1e84359eef3fa4c741d7b961ed3b23b9742
kimplausible0 <andyk1278@gmail.com>
2019-01-05 11:33:31 -0600

    Server side complete. Added middleware. Defined API routes (fetch data) and static routes (load/update views)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 let data = {}
 
 // load the middleware
-app.use(bodyParser.json()))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/assets'))
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,23 +15,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 function slugify(text) {
     return text.toString().toLowerCase().trim()
-		.replace(/\s+/g, '')
+		.replace(/\s+/g, '-')
 		.replace(/[^\w\-]+/g, '')
 		.replace(/\-\-+/g, '-')
 		.replace(/^-+/, '')
-		.replace(/-+/s/, '')
+		.replace(/-+s/, '')
 }
 
 function randomString(count) {
     let text = "";
     const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i  count; i++) {
+    for (let i = 0; i < count; i++) {
 	text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
 }
 
 // API Routes
+
+app.get('/api/notes/:slug', (req, res, next) => {
+    if (data[req.paraams.slug] === undefined) {
+	res.status(404).json({status: 'error'})
+    } else {
+	res.json({data:data[req.params.slug]})
+    }
+})
 
 app.put('/api/notes/:slug', (req, res, next) => {
     if (data[req.params.slug] === undefined) {
@@ -41,3 +49,20 @@ app.put('/api/notes/:slug', (req, res, next) => {
 	res.json({status: "ok"})
     }
 })
+
+app.get('/api/notes', (req, res, next) => res.json({data}))
+
+app.post('/api/notes', (req, res, next) => {
+    const title = randomString(24)
+    res.json({data: data[title] = {slug: title, Title: "Untitled Note"}})
+})
+
+// Define routes static
+
+app.get('/notes/slug', (req, res) => res.sendFile(__dirname + '/views/editor.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+
+// start application
+
+app.listen(3000, () => console.log('App listening on port 3000!'))
+
